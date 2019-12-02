610c287f7467fc5d589eef0d8fbea337abf819b2
venu <venu@venu>
2019-12-02 17:09:11 +0530

    changed

--- githubex/src/githubex/student.java
+++ githubex/src/githubex/student.java
@@ -3,17 +3,29 @@ package githubex;
 public class student {
 	private int id;
 	private String name;
+	private String pass;
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getName() {
 		return name;
 	}
 
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getPass() {
+		return pass;
+	}
 }
